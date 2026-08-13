73f58b493cb11842eda278b72ca4bffcc9b9ab67
CaioSeniuk <caio.seniuk@gmail.com>
2026-08-12 22:16:47 -0300

    refactor: atualizar import de views em reservations/api/urls.py

--- backend/reservations/api/urls.py
+++ backend/reservations/api/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 
-from reservations.views import CommonAreaViewSet, ReservationViewSet
+from reservations.api.views import CommonAreaViewSet, ReservationViewSet
 
 router = DefaultRouter()
 router.register("common-areas", CommonAreaViewSet, basename="commonarea")
